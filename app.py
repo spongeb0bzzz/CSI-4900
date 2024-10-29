aac11687f4e02943e6af7f853510519fd3342bd7
Haoxin Zeng <2015499225@qq.com>
2024-10-29 16:07:57 -0400

    update for url prepossessing

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, jsonify
 import joblib
+from prepossessing import prepossessing_content
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # 加载预训练模型
-phishing_model = joblib.load("path/to/your/model.joblib")
+# phishing_model = joblib.load("path/to/your/model.joblib")
 
 @app.route('/analyze_email', methods=['POST'])
 def analyze_email():
     email_content = request.json.get("email_content")
-    prediction = phishing_model.predict([email_content])[0]
-    result = "Phishing" if prediction == 1 else "Legit"
-    return jsonify({"result": result})
+    email_content = prepossessing_content(email_content)
+
+
+    # prediction = phishing_model.predict([email_content["body"]])[0]
+    # result = "Phishing" if prediction == 1 else "Legit"
+
+
+    return jsonify({"result": email_content["body"]})
 
 if __name__ == '__main__':
     app.run(debug=True)
