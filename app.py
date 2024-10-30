4f2781a033f0894ae34c8d21402e56ec7462fccd
Haoxin Zeng <2015499225@qq.com>
2024-10-30 15:11:02 -0400

    add models for email content

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import joblib
 from prepossessing import prepossessing_content
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
-# 加载预训练模型
-# phishing_model = joblib.load("path/to/your/model.joblib")
+#Load
+
+phishing_model = joblib.load("CSI-4900\model\decision_tree_model_content.joblib")
 
 @app.route('/analyze_email', methods=['POST'])
 def analyze_email():
@@ -14,11 +16,11 @@ def analyze_email():
     email_content = prepossessing_content(email_content)
 
 
-    # prediction = phishing_model.predict([email_content["body"]])[0]
+    prediction = phishing_model.predict([email_content["body"]])[0]
     # result = "Phishing" if prediction == 1 else "Legit"
 
 
-    return jsonify({"result": email_content["body"]})
+    return jsonify({"result": prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
