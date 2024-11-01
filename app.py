142a038ffc02fb9f9f8258a8d1b62094ff215db5
Nassim Tiraoui <ntira053@uottawa.ca>
2024-11-01 10:36:02 -0400

    added new models for email prediction

--- app.py
+++ app.py
@@ -4,21 +4,14 @@ from prepossessing import prepossessing_content
 from flask_cors import CORS
 import numpy as np
 from eml_feature import extract_eml
-# import warnings
-# from sklearn.exceptions import InconsistentVersionWarning
-
-# # Suppress InconsistentVersionWarning
-# warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
 
 app = Flask(__name__)
 CORS(app)
-#Load
 
-# phishing_model = joblib.load("CSI-4900\model\decision_tree_model_content.joblib")
-
-model = joblib.load('CSI-4900\model\models email content\stacking_model (1).joblib')
-# model = joblib.load('CSI-4900\model\models email content\svm_model (1).joblib')
-vectorizer = joblib.load('CSI-4900\\model\\vectorizer.joblib')
+# Load models
+model = joblib.load('model/new email models/stacking_model.joblib')
+vectorizer = joblib.load('model/new email models/vectorizer.joblib')
+scaler = joblib.load('model/new email models/scaler_model.joblib')
 
 @app.route('/analyze_email', methods=['POST'])
 def analyze_email():
@@ -46,12 +39,12 @@ def analyze_email():
     
     # Preprocess the email body
     X_text = vectorizer.transform([email_body])
+    
+    # Apply scaling
+    X_scaled = scaler.transform(X_text.toarray())
 
     # Make a prediction
-    prediction_proba_model = model.predict_proba(X_text)[0]
-
-    # Output the model prediction probabilities
-    # print(f"Model Prediction probabilities: {prediction_proba_model}")
+    prediction_proba_model = model.predict_proba(X_scaled)[0]
 
     # Define a prediction function for SHAP
     def model_predict(X):
@@ -63,10 +56,10 @@ def analyze_email():
     # Determine the prediction label
     prediction_label = "Spam" if accuracy_model > 0.5 else "Not Spam"
 
-
     return jsonify({
-    "Model_Accuracy": f"{max(prediction_proba_model[0], prediction_proba_model[1]) * 100:.2f}%",
-    "Prediction": prediction_label})
+        "Model_Accuracy": f"{max(prediction_proba_model[0], prediction_proba_model[1]) * 100:.2f}%",
+        "Prediction": prediction_label
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
