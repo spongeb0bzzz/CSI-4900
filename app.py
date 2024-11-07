a74493d0e85406863630b81021c9f5657c32a844
Haoxin Zeng <2015499225@qq.com>
2024-11-07 15:24:31 -0500

    update for eml extraction and final output

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 from preprocessing import preprocessing_content
 from flask_cors import CORS
 import numpy as np
-from eml_feature import extract_eml
+from eml_feature import extract_eml,extract_eml_body
 import logging
 from get_URL_features import extract_links,extract_features
 import pandas as pd
@@ -44,15 +44,23 @@ def analyze_email():
         if file.filename == '':
             return jsonify({"error": "No file selected"}), 400
         # Read and decode content from the file directly
-        raw_content = file.read()
-        email_content = raw_content.decode('utf-8', errors='replace') if isinstance(raw_content, bytes) else raw_content
-        # Extract content from the uploaded .eml file
-        email_content = extract_eml(email_content)
+        try:
+            email_content = extract_eml_body(file)
 
-        # logging.info(f'Email content: {email_content}')
-        
-        email_body = email_content.get("body_plain", "")
-        email_info = preprocessing_content(email_body)
+        except Exception as e:
+            try:
+                raw_content = file.read()
+                email_content = raw_content.decode('utf-8', errors='replace') if isinstance(raw_content, bytes) else raw_content
+                # Extract content from the uploaded .eml file
+                # logging.info(f'Email content: {email_content}')
+                
+                email_body = email_content.get("body_plain", "")
+                email_info = preprocessing_content(email_body)
+
+            except Exception as e:
+                logging.error(f"Error extracting email content with extract_eml_body: {e}")
+
+        email_info = preprocessing_content(email_content)
         email_body = email_info.get("body", "")
         
         
@@ -71,13 +79,19 @@ def analyze_email():
         # Use the same function to preprocess text input as EML files
         email_info = preprocessing_content(email_content)
         email_body = email_info.get("body", "")
-        logging.info(f'Email info: {email_info}')
+        # logging.info(f'Email info: {email_info}')
         # Log the preprocessed email body
         logging.info(f'Preprocessed Email Body: {email_body}')
 
     if not email_body:
         return jsonify({"error": "Email body is empty after processing"}), 400
     
+
+    ############################################################################################################################################################
+
+    ##GET THE LINKS From Email content
+
+    ############################################################################################################################################################
     links = extract_links(email_body)
     logging.info(f'Contain links:{links}')
 
@@ -104,11 +118,11 @@ def analyze_email():
             predictions_url.append({     
                 'url': url,
                 'prediction_label': prediction_label_url,
-                'accuracy_model': f"{float(accuracy_model_url) * 100:.2f}%"
+                'accuracy_model': f"{max(prediction_proba_model_url[0], prediction_proba_model_url[1]) * 100:.2f}%",
+                'spam_rate': accuracy_model_url
             })
 
     logging.info(f'Url prediction: {predictions_url}')
-
 
     # Preprocess the email body
     X_text = vectorizer.transform([email_body])
@@ -118,6 +132,7 @@ def analyze_email():
 
     # Make a prediction
     prediction_proba_model = model.predict_proba(X_scaled)[0]
+    logging.info(f'PPM: {prediction_proba_model}')
 
     # Calculate individual accuracies
     accuracy_model = prediction_proba_model[1]  # Probability of being spam
@@ -125,10 +140,30 @@ def analyze_email():
     # Determine the prediction label
     prediction_label = "Spam" if accuracy_model > 0.5 else "Not Spam"
 
+    #Calculate the final output
+    accuracies = [float(item['spam_rate']) for item in predictions_url]
+
+    if accuracies:
+        average_accuracy_list = sum(accuracies)/len(accuracies)
+    else:
+        average_accuracy_list = 0
+
+    final_output = (0.7 * average_accuracy_list +0.3* accuracy_model) if average_accuracy_list != 0 else accuracy_model
+    logging.info(f'accuracies: {accuracies}')
+    logging.info(f'aal: {average_accuracy_list}')
+    logging.info(f'AM: {accuracy_model}')
+    logging.info(f'Final out: {final_output}')
+
+    final_label = "Spam" if final_output >  0.5 else "Not Spam"
+
+    final_accuracy = final_output if final_label == 'Spam' else 1-final_output
+
     response_data = {
     "Model_Accuracy": f"{max(prediction_proba_model[0], prediction_proba_model[1]) * 100:.2f}%",
     "Prediction": prediction_label,
-    "links": predictions_url  # Ensure this key matches with JavaScript
+    "links": predictions_url,  # Ensure this key matches with JavaScript
+    "output": f"{final_accuracy *100:.2f}%",
+    "OutputLabel": final_label
 }
     logging.info(f'Response Data: {response_data}')
     return jsonify(response_data)
